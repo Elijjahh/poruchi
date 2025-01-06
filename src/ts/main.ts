79ae2d478ebd3b238bc3c449e61521c57430b4dd
Илья Белов <ibelov292005@gmail.com>
2025-01-06 09:22:49 +0300

    feat: добавил header и секцию map

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,42 @@
 import '../scss/main.scss';
+
+const elementToShowHide = document.querySelector('.header');
+
+function checkScrollPosition() {
+  const scrollY = window.scrollY;
+
+  if (elementToShowHide) {
+    if (scrollY > 200) {
+      elementToShowHide.classList.add('active');
+    } else {
+      elementToShowHide.classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', checkScrollPosition);
+
+checkScrollPosition();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.header__hamburger');
+  const menu = document.querySelector('.header__menu');
+
+  if (hamburger && menu) {
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+
+      if (target.getAttribute('href') == '#faq' && menu.classList.contains('active')) {
+        menu.classList.remove('active');
+      }
+
+      if (!menu.contains(target) && menu.classList.contains('active')) {
+        menu.classList.remove('active');
+      }
+
+      if (hamburger.contains(target) && !menu.classList.contains('active')) {
+        menu.classList.add('active');
+      }
+    });
+  }
+});
