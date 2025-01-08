308e06e93ff19b75e2a98eafa121c8e25ea6316c
Илья Белов <ibelov292005@gmail.com>
2025-01-08 22:50:14 +0300

    fix: починил скролл по кнопкам

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,24 +4,6 @@ import Swiper from 'swiper';
 // @ts-expect-error импорт css, не знаю, чего он ругается
 import 'swiper/css';
 
-const elementToShowHide = document.querySelector('.header');
-
-function checkScrollPosition() {
-  const scrollY = window.scrollY;
-
-  if (elementToShowHide) {
-    if (scrollY > 200) {
-      elementToShowHide.classList.add('active');
-    } else {
-      elementToShowHide.classList.remove('active');
-    }
-  }
-}
-
-window.addEventListener('scroll', checkScrollPosition);
-
-checkScrollPosition();
-
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.header__hamburger');
   const menu = document.querySelector('.header__menu');
@@ -42,12 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.add('active');
       }
     });
-
-    document.addEventListener('scroll', (_event) => {
-      if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-      }
-    });
   }
 });
 
@@ -66,13 +42,28 @@ new Swiper('#places .swiper', {
   spaceBetween: 12,
 });
 
-const accordions = document.getElementsByClassName('faq__accordion-item');
+// @ts-expect-error no types there
+import SnapSlider from '@tannerhodges/snap-slider';
 
-for (let i = 0; i < accordions.length; i++) {
-  accordions[i].addEventListener('click', () => {
-    for (const ac of accordions) {
-      ac.classList.remove('active');
-    }
-    accordions[i].classList.toggle('active');
-  });
-}
+const elementToShowHide = document.querySelector('.header');
+const slider = new SnapSlider('.main', {
+  id: 'sections',
+  slides: '',
+  start: 1,
+  loop: false,
+  on: {
+    load: () => {},
+    change: () => {
+      if (!elementToShowHide) return;
+      const menu = document.querySelector('.header__menu');
+      if (menu?.classList.contains('active')) {
+        menu.classList.remove('active');
+      }
+      if (slider.getCurrentSlide().id !== 'promo') {
+        elementToShowHide.classList.add('active');
+      } else {
+        elementToShowHide.classList.remove('active');
+      }
+    },
+  },
+});
